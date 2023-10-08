2c676dbc7eec25bfeafe380eaa4180c1aceb4196
Pedro Hernandez <969572632.max@gmail.com>
2023-10-08 16:20:03 -0500

    secod commit

--- js/mapa.js
+++ js/mapa.js
@@ -1,18 +1,33 @@
-var map = L.map('map').setView([8.93314799529361, 24.890625], 4);
+var map = L.map('map').setView([0, 0], 2); 
 
-        // Agrega un mapa base (por ejemplo, OpenStreetMap)
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-        // Datos de ejemplo de puntos con coordenadas y nombres
-        var points = [
-            { lat: 8.93314799529361, lon: 24.890625, name: "Gas Metano" },
-            // Agrega más puntos aquí en el mismo formato
-        ];
 
-        // Agrega los puntos al mapa con iconos predeterminados de Leaflet
-        points.forEach(function(point) {
-            L.marker([point.lat, point.lon]).addTo(map)
-                .bindPopup(point.name); // Agrega un popup con el nombre del punto
-        });
+var continents = ["África", "Norteamérica", "Sudamérica", "Europa", "Oceanía", "Asia"];
+var gasTypes = ["Gas Metano", "Gas CO2", "Gas Oxígeno", "Gas Nitrógeno", "Gas Helio", "Gas Hidrógeno"];
+
+var points = [];
+
+for (var i = 0; i < 100; i++) {
+    var randomContinent = continents[Math.floor(Math.random() * continents.length)];
+    var randomGasType = gasTypes[Math.floor(Math.random() * gasTypes.length)];
+    var randomLat = (Math.random() * 180) - 90; 
+    var randomLon = (Math.random() * 360) - 180; 
+
+    points.push({
+        lat: randomLat,
+        lon: randomLon,
+        name: "Punto " + (i + 1),
+        gasType: randomGasType,
+        continent: randomContinent
+    });
+}
+
+
+points.forEach(function(point) {
+    
+    L.marker([point.lat, point.lon]).addTo(map)
+        .bindPopup('Nombre: ' + point.name + '<br>Tipo de Gas: ' + point.gasType + '<br>Continente: ' + point.continent); 
+});
